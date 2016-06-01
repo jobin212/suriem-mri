534800e1f01744b6585ec1188d1dbc782a7cae91
joe <jobin212@gmail.com>
2016-06-01 14:14:36 -0400

    updated excpi file

--- excpi.m
+++ excpi.m
@@ -1,21 +1,30 @@
 N = 50;
 
-coefficients = 1:2*N+1
+%coefficients = 1:2*N+1;
 
-for i = 1:(2*N+1)
-    k = coefficients(i) - N -1;
-    
-    %%insert formula here
-    coefficients(i) = ((-1)^(k)) / (pi^(2)*k);
-end
-  
+
+k = -N:N;
+coefficients = (sin(k*pi/2) ./ (pi^(2)*k));
+
 %replace k = 0 with 0 (evalute original integral with k = 0)
-coefficients(N+1) = 0
+coefficients(N+1) = 0;
+
 
 [reconstruction, domain] = ComputeFourierReconstruction(coefficients);
 
 
-%%
+%compute pi(x)
+region1 = (domain <= -pi/2 | domain >= pi/2);
+region2 = (domain > -pi/2 & domain < pi/2);
+
+p = zeros(size(domain));
+p(region2) = 1/ pi;
+
+
+%calculate error
+error = abs(p - reconstruction);
+
 
 plot(domain, reconstruction);
-
+figure;
+plot(domain, error);
