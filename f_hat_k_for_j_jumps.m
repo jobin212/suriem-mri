db9f90c2c3974b81e9168293fbaa123ed6bb1304
joe <jobin212@gmail.com>
2016-06-02 15:16:11 -0400

    fixed Jump5r, added general jump calculator

---
+++ f_hat_k_for_j_jumps.m
@@ -0,0 +1,22 @@
+%calculate the coefficients for j jumps
+N = 50;
+k = -N:N;
+
+%jump discontinuities
+J = 1;
+x = zeros(J);
+%x(0) = x_0, x(1) = x_1, ...
+
+%define jump function
+r_jump = zeros(size(x));
+%r_jump(0) = [r](x_0), r_jump(1) = [r](x_1)
+
+
+%calculate coefficient estimates
+coefficients_est_k = zeros(size(k));
+for y = size(k)
+    for z = size(r_jump)
+        coefficients_est_k(y) = coefficients_est_k(y) + (r_jump(z) * exp(-1i* x(z)*y))/(2i*pi*y);
+    end
+end
+        
